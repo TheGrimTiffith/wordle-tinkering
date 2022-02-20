87a2eeac5fb5d07c971e7e6d7e5a23d214a7ebc8
TheGrimTiffith <tim.griffith.dev@gmail.com>
2022-02-19 16:13:58 -0800

    3rd time lucky trying to fix relative path based loading of the dictionary file!

--- src/main/kotlin/com/fafflegriff/wordle/WordleDictionary.kt
+++ src/main/kotlin/com/fafflegriff/wordle/WordleDictionary.kt
@@ -4,7 +4,7 @@ import java.io.File
 
 class WordleDictionary {
     companion object Instance {
-        private val dictionary = File("src/main/resources/dictionary.txt").readLines()
+        private val dictionary = WordleDictionary::class.java.getResourceAsStream("/dictionary.txt").bufferedReader().readLines()
         fun getDictionary(): List<String> = dictionary
     }
 }
